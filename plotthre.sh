5bd0d2570f44a2000cadbef633c5550c2e50e1d3
rhit-chenz20 <chenz20@rose-hulman.edu>
2022-08-08 14:23:53 -0400

    debugged

--- plotthre.sh
+++ plotthre.sh
@@ -1,3 +1,18 @@
+declare date="Aug8"
+declare c=0
+declare max=40
+
+                        mkdir ../result/${date}/plot/Threshold/mating_length
+                        mkdir ../result/${date}/plot/Threshold/male_sigma
+
+                        mkdir ../result/${date}/plot/Threshold/female_sigma
+
+                        mkdir ../result/${date}/plot/Threshold/female_mean
+                        mkdir ../result/${date}/plot/Threshold/fitness_function
+                        mkdir ../result/${date}/plot/Threshold/cost
+
+
+
 for MS in 1 5 10
 do
     for FS in 1 5 10
@@ -11,7 +26,7 @@ do
                     for ML in 1 5 10 20
                     do
                         # ploting mating length
-                        mkdir ../result/${date}/plot/Threshold/mating_length
+
                         let "c+=1"
                         python3 plot.py -inf ../result/${date}/CSV/Threshold/ml_*_ms_${MS}_fs_${FS}_fmu_${FM}_fit_${FIT}_cost_${COST}_*.csv -out ../result/${date}/plot/Threshold/mating_length/ms_${MS}_fs_${FS}_fmu_${FM}_fit_${FIT}_cost_${COST} -t 0 &
                         echo "finished plotting $c"
@@ -23,7 +38,7 @@ do
                             fi
 
                         # ploting male sigma
-                        mkdir ../result/${date}/plot/Threshold/male_sigma
+
                         let "c+=1"
                         python3 plot.py -inf ../result/${date}/CSV/Threshold/ml_${ML}_ms_*_fs_${FS}_fmu_${FM}_fit_${FIT}_cost_${COST}_*.csv -out ../result/${date}/plot/Threshold/male_sigma/ml_${ML}_fs_${FS}_fmu_${FM}_fit_${FIT}_cost_${COST} -t 0 &
                         echo "finished plotting $c"
@@ -34,7 +49,7 @@ do
                             fi
 
                         # ploting female sigma
-                        mkdir ../result/${date}/plot/Threshold/female_sigma
+
                         let "c+=1"
                         python3 plot.py -inf ../result/${date}/CSV/Threshold/ml_${ML}_ms_${MS}_fs_*_fmu_${FM}_fit_${FIT}_cost_${COST}_*.csv -out ../result/${date}/plot/Threshold/female_sigma/ml_${ML}_ms_${MS}_fmu_${FM}_fit_${FIT}_cost_${COST} -t 0 &
                         echo "finished plotting $c"
@@ -45,7 +60,7 @@ do
                             fi
 
                         # ploting female mu
-                        mkdir ../result/${date}/plot/Threshold/female_mean
+
                         let "c+=1"
                         python3 plot.py -inf ../result/${date}/CSV/Threshold/ml_${ML}_ms_${MS}_fs_${FS}_fmu_*_fit_${FIT}_cost_${COST}_*.csv -out ../result/${date}/plot/Threshold/female_mean/ml_${ML}_ms_${MS}_fs_${FS}_fit_${FIT}_cost_${COST} -t 0 &
                         echo "finished plotting $c"
@@ -56,7 +71,7 @@ do
                             fi
 
                         # ploting fitness function
-                        mkdir ../result/${date}/plot/Threshold/fitness_function
+
                         let "c+=1"
                         python3 plot.py -inf ../result/${date}/CSV/Threshold/ml_${ML}_ms_${MS}_fs_${FS}_fmu_${FM}_fit_*_cost_${COST}_*.csv -out ../result/${date}/plot/Threshold/fitness_function/ml_${ML}_ms_${MS}_fs_${FS}_fmu_${FM}_cost_${COST} -t 0 &
                         echo "finished plotting $c"
@@ -67,7 +82,7 @@ do
                             fi
 
                         # ploting cost
-                        mkdir ../result/${date}/plot/Threshold/cost
+
                         let "c+=1"
                         python3 plot.py -inf ../result/${date}/CSV/Threshold/ml_${ML}_ms_${MS}_fs_${FS}_fmu_${FM}_fit_${FIT}_cost_*_*.csv -out ../result/${date}/plot/Threshold/cost/ml_${ML}_ms_${MS}_fs_${FS}_fmu_${FM}_fit_${FIT} -t 0 &
                         echo "finished plotting $c"
