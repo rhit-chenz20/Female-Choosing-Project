a78493bbcecaae86fb488b4bf8ebcb8280879923
rhit-chenz20 <chenz20@rose-hulman.edu>
2022-08-15 09:39:51 -0400

    added  missing done

--- plotgeno.sh
+++ plotgeno.sh
@@ -51,17 +51,18 @@ do
                     do
                         for MR in 0 1 3 5 10 20
                         do
-                    # ploting mating length
+                        # ploting mating length
 
-                    let "c+=1"
-                    python3 plot.py -inf ../result/${date}/CSV/Learning/ml_${ML}_ms_${MS}_fit_${FIT}_cost_${COST}_memory_*_*.csv -out ../result/${date}/plot/Learning/memory/ml_${ML}ms_${MS}_fit_${FIT}_cost_${COST} -t 1 -ing ../result/${date}/CSV/Learning/last_ml_${ML}_ms_${MS}_fit_${FIT}_cost_${COST}_memory_*_*.csv
-                    echo "finished plotting $c"
-                    
-                    if c>$max
-                    then
-                        wait
-                        let "c=0"
-                    fi
+                        let "c+=1"
+                        python3 plot.py -inf ../result/${date}/CSV/Learning/ml_${ML}_ms_${MS}_fit_${FIT}_cost_${COST}_memory_*_*.csv -out ../result/${date}/plot/Learning/memory/ml_${ML}ms_${MS}_fit_${FIT}_cost_${COST} -t 1 -ing ../result/${date}/CSV/Learning/last_ml_${ML}_ms_${MS}_fit_${FIT}_cost_${COST}_memory_*_*.csv
+                        echo "finished plotting $c"
+                        
+                        if ((c>$max))
+                        then
+                            wait
+                            let "c=0"
+                        fi
+                    done
                 done
             done
         done
