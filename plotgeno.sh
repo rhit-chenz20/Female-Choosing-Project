44568c7352f53190a71811eac3f288745a274f56
rhit-chenz20 <chenz20@rose-hulman.edu>
2022-08-15 09:38:36 -0400

    changed if

--- plotgeno.sh
+++ plotgeno.sh
@@ -57,11 +57,11 @@ do
                     python3 plot.py -inf ../result/${date}/CSV/Learning/ml_${ML}_ms_${MS}_fit_${FIT}_cost_${COST}_memory_*_*.csv -out ../result/${date}/plot/Learning/memory/ml_${ML}ms_${MS}_fit_${FIT}_cost_${COST} -t 1 -ing ../result/${date}/CSV/Learning/last_ml_${ML}_ms_${MS}_fit_${FIT}_cost_${COST}_memory_*_*.csv
                     echo "finished plotting $c"
                     
-                    if ((c>$max))
-                        then
-                            wait
-                            let "c=0"
-                        fi
+                    if c>$max
+                    then
+                        wait
+                        let "c=0"
+                    fi
                 done
             done
         done
